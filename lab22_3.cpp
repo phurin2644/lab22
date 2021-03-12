fda681c627719c178e11990c2ed0b7719c6ee05e
phurin2644 <phus.2644@gmail.com>
2021-03-12 17:41:15 +0700

    com

--- lab22_3.cpp
+++ lab22_3.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
+#include<string>
+#include<set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int* x,unsigned int y){
+	unsigned int count;
+	set<int> myset;
+	for(unsigned int i=0;i<y;i++){
+		myset.insert(x[i]);
+	}
+	count = myset.size();
 
-int main(){	
-	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
-	
-	cout << "There are " << count(data,sizeof(data)/sizeof(int)); 
-	cout << " different numbers in data.";
-	
-	return 0;
+	return count;
 }
